eaaddf9931d9d2ea914848c4bd803991cb4a5b79
Marco Roth <marco.roth@intergga.ch>
2023-03-29 03:29:53 +0200

    Add tests for `action` and `action_all`

---
+++ test/streams/tag_builder_test.rb
@@ -0,0 +1,15 @@
+require "tag_builder_test_case"
+
+class Turbo::TagBuilderTest < TagBuilderTestCase
+  test "action with name and target as args and content as kwarg" do
+    stream = %(<turbo-stream action="custom_action" target="custom_target"><template>Custom Content</template></turbo-stream>)
+
+    assert_equal stream, turbo_stream.action("custom_action", "custom_target", content: "Custom Content")
+  end
+
+  test "action_all with name and targets as args and content as kwarg" do
+    stream = %(<turbo-stream action="custom_action" targets="custom_targets"><template>Custom Content</template></turbo-stream>)
+
+    assert_equal stream, turbo_stream.action_all("custom_action", "custom_targets", content: "Custom Content")
+  end
+end
